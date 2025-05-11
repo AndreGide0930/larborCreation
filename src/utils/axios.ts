2de8ef52d21fce9f89b5548dc48774502fd1ac14
AndreGide0930 <5zjxcz9c@pp.shaopping.com>
2025-05-11 21:25:52 +0800

    Update Tasks view to render tasks from API

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,38 +1,38 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// const instance = axios.create({
-//   baseURL: '/api',  // 设置基础URL
-//   timeout: 10000    // 设置超时时间
-// })
+const instance = axios.create({
+  baseURL: '/api',  // 设置基础URL
+  timeout: 10000    // 设置超时时间
+})
 
-// // 请求拦截器
-// instance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token')
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`
-//     }
-//     return config
-//   },
-//   (error) => {
-//     return Promise.reject(error)
-//   }
-// )
+// 请求拦截器
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 
-// // 响应拦截器
-// instance.interceptors.response.use(
-//   (response) => {
-//     return response
-//   },
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       // token过期或无效，清除本地token
-//       localStorage.removeItem('token')
-//       // 可以在这里添加重定向到登录页面的逻辑
-//       window.location.href = '/login'
-//     }
-//     return Promise.reject(error)
-//   }
-// )
+// 响应拦截器
+instance.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      // token过期或无效，清除本地token
+      localStorage.removeItem('token')
+      // 可以在这里添加重定向到登录页面的逻辑
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
 
-// export default instance
+export default instance
